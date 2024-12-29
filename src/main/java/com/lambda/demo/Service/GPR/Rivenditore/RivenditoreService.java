8e0475185726a7551ad6b1112eb2912f9eb9163f
Raffaele <raffaelecoppola603@gmail.com>
2024-12-29 21:55:07 +0100

    modifica rivenditore service

--- src/main/java/com/lambda/demo/Service/GPR/Rivenditore/RivenditoreService.java
+++ src/main/java/com/lambda/demo/Service/GPR/Rivenditore/RivenditoreService.java
@@ -1,7 +1,11 @@
-package com.lambda.demo.Service.GPR.Acquirente;
+package com.lambda.demo.Service.GPR.Rivenditore;
+
+import com.lambda.demo.Entity.GPR.RivenditoreEntity;
 
 public interface RivenditoreService {
     void signupRivenditore(String ragioneSociale, String partitaIVa, String email, String password) throws Exception;
 
     void loginRivenditore(String email, String password) throws Exception;
+
+    int updateRivenditore(RivenditoreEntity rivenditoreEntity);
 }
