61d19587f549c850f5349c87bb3ecad4c572a8b9
Raffaele <raffaelecoppola603@gmail.com>
2025-01-07 16:13:30 +0100

    aggiunta logica di ricerca dei prodotti e modifica di AcquirenteService

--- src/main/java/com/lambda/demo/Service/GPR/Acquirente/AcquirenteService.java
+++ src/main/java/com/lambda/demo/Service/GPR/Acquirente/AcquirenteService.java
@@ -3,7 +3,7 @@ package com.lambda.demo.Service.GPR.Acquirente;
 import com.lambda.demo.Entity.GPR.AcquirenteEntity;
 
 public interface AcquirenteService {
-    void signupAcquirente(String nome, String cognome, String email, String password) throws Exception;
+    void signupAcquirente(String nome, String cognome, String email, String password, String confermaPassword) throws Exception;
 
     void loginAcquirente(String email, String password) throws Exception;
 
